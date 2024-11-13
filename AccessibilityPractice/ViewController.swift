437251c6d91c000f7b6da0d9e52f4727df7edece
yongjun18 <yongjun18976@gmail.com>
2024-11-13 20:24:03 +0900

    초기 세팅 진행

---
+++ AccessibilityPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessibilityPractice
+//
+//  Created by yongjun18 on 11/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
