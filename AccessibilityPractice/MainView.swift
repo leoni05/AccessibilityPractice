af93c9b6ffc0ea0a724e3a6135a7fff3e61c536e
yongjun18 <yongjun18976@gmail.com>
2024-11-17 19:21:13 +0900

    MainView 세팅

---
+++ AccessibilityPractice/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  AccessibilityPractice
+//
+//  Created by yongjun18 on 11/17/24.
+//
+
+import Foundation
+import UIKit
+
+class MainView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
