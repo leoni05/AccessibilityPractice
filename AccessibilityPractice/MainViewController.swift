452d96539022ca874be87985aa203dc10c7f2c36
yongjun18 <yongjun18976@gmail.com>
2024-11-17 21:09:55 +0900

    PresentaionPart 구현

--- AccessibilityPractice/MainViewController.swift
+++ AccessibilityPractice/MainViewController.swift
@@ -8,6 +8,22 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    private var presentationParts = Array<PresentationPart>()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        presentationParts.append(PresentationPart(tabIconImage: UIImage(named: "square.and.arrow.up"), pages: []))
+        presentationParts.append(PresentationPart(tabIconImage: UIImage(named: "square.and.arrow.down"), pages: []))
+        presentationParts.append(PresentationPart(tabIconImage: UIImage(named: "square.and.arrow.up.on.square"), pages: []))
+        presentationParts.append(PresentationPart(tabIconImage: UIImage(named: "square.and.arrow.down.on.square"), pages: []))
+        presentationParts.append(PresentationPart(tabIconImage: UIImage(named: "rectangle.portrait.and.arrow.right"), pages: []))
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ AccessibilityPractice/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  AccessibilityPractice
+//
+//  Created by yongjun18 on 11/17/24.
+//
+
+import Foundation
+import UIKit
+
+struct PresentationPart {
+    var tabIconImage: UIImage?
+    var pages: [UIViewController]
+}
