7c0b55f89ee9d82fdaa0be9c5a724519c9f3ecdc
yongjun18 <yongjun18976@gmail.com>
2024-11-18 20:59:05 +0900

    사이드 버튼 배치

--- AccessibilityPractice/MainViewController.swift
+++ AccessibilityPractice/MainViewController.swift
@@ -6,19 +6,22 @@
 //
 
 import UIKit
+import PinLayout
 
 class MainViewController: UIViewController {
     
     private var presentationParts = Array<PresentationPart>()
+    private var btnContainerView = UIView()
+    private var sideButtons = Array<UIButton>()
     
     init() {
         super.init(nibName: nil, bundle: nil)
         
-        presentationParts.append(PresentationPart(tabIconImage: UIImage(named: "square.and.arrow.up"), pages: []))
-        presentationParts.append(PresentationPart(tabIconImage: UIImage(named: "square.and.arrow.down"), pages: []))
-        presentationParts.append(PresentationPart(tabIconImage: UIImage(named: "square.and.arrow.up.on.square"), pages: []))
-        presentationParts.append(PresentationPart(tabIconImage: UIImage(named: "square.and.arrow.down.on.square"), pages: []))
-        presentationParts.append(PresentationPart(tabIconImage: UIImage(named: "rectangle.portrait.and.arrow.right"), pages: []))
+        presentationParts.append(PresentationPart(tabIconImage: UIImage(systemName: "square.and.arrow.up"), pages: []))
+        presentationParts.append(PresentationPart(tabIconImage: UIImage(systemName: "square.and.arrow.down"), pages: []))
+        presentationParts.append(PresentationPart(tabIconImage: UIImage(systemName: "square.and.arrow.up.on.square"), pages: []))
+        presentationParts.append(PresentationPart(tabIconImage: UIImage(systemName: "square.and.arrow.down.on.square"), pages: []))
+        presentationParts.append(PresentationPart(tabIconImage: UIImage(systemName: "rectangle.portrait.and.arrow.right"), pages: []))
     }
 
     required init(coder aDecoder: NSCoder) {
@@ -27,11 +30,28 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .black
+        
+        self.view.addSubview(btnContainerView)
+        
+        for idx in presentationParts.indices {
+            let button = UIButton()
+            button.setImage(presentationParts[idx].tabIconImage, for: .normal)
+            button.tintColor = .white
+            btnContainerView.addSubview(button)
+            sideButtons.append(button)
+        }
     }
-
-    override func loadView() {
-        self.view = MainView()
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        for idx in sideButtons.indices {
+            if idx == 0 { sideButtons[idx].pin.top().left().size(60) }
+            else { sideButtons[idx].pin.below(of: sideButtons[idx-1], aligned: .center).size(60) }
+        }
+        btnContainerView.pin.left(self.view.pin.safeArea).vCenter().wrapContent()
     }
+    
 }
 
