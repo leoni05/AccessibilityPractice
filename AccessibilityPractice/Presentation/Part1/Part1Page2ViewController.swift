422bd7d9ef6ec44b9366a2f7ff6f7795a222f922
yongjun18 <yongjun18976@gmail.com>
2024-11-22 16:56:10 +0900

    title, subtitle 세팅

--- AccessibilityPractice/Presentation/Part1/Part1Page2ViewController.swift
+++ AccessibilityPractice/Presentation/Part1/Part1Page2ViewController.swift
@@ -13,6 +13,9 @@ class Part1Page2ViewController: PresentationViewController {
     
     // MARK: - Properties
     
+    private var mainContentView = UIView()
+    private var titleLabel = UILabel()
+    private var subtitleLabel = UILabel()
     
  
     // MARK: - Life Cycle
@@ -20,10 +23,25 @@ class Part1Page2ViewController: PresentationViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.addSubview(mainContentView)
+        
+        titleLabel.text = "Apple의 접근성"
+        titleLabel.font = .systemFont(ofSize: 25, weight: .semibold)
+        titleLabel.textColor = .white
+        mainContentView.addSubview(titleLabel)
+        
+        subtitleLabel.text = "Apple이 지원하는 접근성 기능"
+        subtitleLabel.font = .systemFont(ofSize: 15)
+        subtitleLabel.textColor = .white
+        mainContentView.addSubview(subtitleLabel)
+        
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
+        mainContentView.pin.left(20).right().vertically()
+        titleLabel.pin.top(self.view.pin.safeArea).horizontally().marginTop(40).sizeToFit(.width)
+        subtitleLabel.pin.below(of: titleLabel).horizontally().marginTop(5).sizeToFit(.width)
     }
 }
