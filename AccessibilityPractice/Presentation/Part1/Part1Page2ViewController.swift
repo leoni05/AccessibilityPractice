bf397c904ef6674660d9909e24d731e82eb6b8e9
yongjun18 <yongjun18976@gmail.com>
2024-11-24 02:38:11 +0900

    Contents 화면 세팅

--- AccessibilityPractice/Presentation/Part1/Part1Page2ViewController.swift
+++ AccessibilityPractice/Presentation/Part1/Part1Page2ViewController.swift
@@ -17,6 +17,14 @@ class Part1Page2ViewController: PresentationViewController {
     private var titleLabel = UILabel()
     private var subtitleLabel = UILabel()
     
+    private var contentItems: Array<String> = [
+        "1. 접근성이란 무엇인가",
+        "2. Apple의 접근성",
+        "3. iOS 접근성 구현 방법(Voice Over)",
+        "4. 발표를 마치며"
+    ]
+    private var contentItemLabels = Array<UILabel>()
+    private var contentItemContainerView = UIView()
  
     // MARK: - Life Cycle
     
@@ -35,6 +43,15 @@ class Part1Page2ViewController: PresentationViewController {
         subtitleLabel.textColor = .white
         mainContentView.addSubview(subtitleLabel)
         
+        for i in contentItems.indices {
+            let contentItemLabel = UILabel()
+            contentItemLabel.text = contentItems[i]
+            contentItemLabel.font = .systemFont(ofSize: 20)
+            contentItemLabel.textColor = .white
+            contentItemLabels.append(contentItemLabel)
+            contentItemContainerView.addSubview(contentItemLabel)
+        }
+        mainContentView.addSubview(contentItemContainerView)
     }
     
     override func viewDidLayoutSubviews() {
@@ -43,5 +60,17 @@ class Part1Page2ViewController: PresentationViewController {
         mainContentView.pin.left(20).right(self.view.pin.safeArea).vertically()
         titleLabel.pin.top(self.view.pin.safeArea).horizontally().marginTop(40).sizeToFit(.width)
         subtitleLabel.pin.below(of: titleLabel).horizontally().marginTop(5).sizeToFit(.width)
+        
+        contentItemContainerView.pin.horizontally()
+        for i in contentItemLabels.indices {
+            if i == 0 {
+                contentItemLabels[i].pin.top().horizontally().sizeToFit(.width)
+            }
+            else {
+                contentItemLabels[i].pin.below(of: contentItemLabels[i-1], aligned: .left)
+                    .right().marginTop(15).sizeToFit(.width)
+            }
+        }
+        contentItemContainerView.pin.wrapContent().left().vCenter()
     }
 }
