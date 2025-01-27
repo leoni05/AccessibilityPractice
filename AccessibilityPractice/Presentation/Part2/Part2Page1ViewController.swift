a7b62fdcc6a4e194d2e00ab2f5d941cba7d8444e
yongjun18 <yongjun18976@gmail.com>
2025-01-27 16:54:49 +0900

    part2 page1 배경 이미지 세팅

--- AccessibilityPractice/Presentation/Part2/Part2Page1ViewController.swift
+++ AccessibilityPractice/Presentation/Part2/Part2Page1ViewController.swift
@@ -7,10 +7,46 @@
 
 import Foundation
 import UIKit
+import PinLayout
 
 class Part2Page1ViewController: PresentationViewController {
     
+    // MARK: - Properties
+    
+    private var backgroundImageView = UIImageView()
+    private var backgroundGradientLayerH = CAGradientLayer()
+    private var backgroundGradientLayerV = CAGradientLayer()
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        backgroundImageView.image = UIImage(named: "BackgroundImage1")
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.layer.masksToBounds = true
+        self.view.addSubview(backgroundImageView)
+        
+        let colors: [CGColor] = [
+           .init(red: 0, green: 0, blue: 0, alpha: 1),
+           .init(red: 0, green: 0, blue: 0, alpha: 0.01),
+        ]
+        backgroundGradientLayerH.colors = colors
+        backgroundGradientLayerH.startPoint = CGPoint(x: 0.0, y: 0.5)
+        backgroundGradientLayerH.endPoint = CGPoint(x: 1.0, y: 0.5)
+        backgroundImageView.layer.addSublayer(backgroundGradientLayerH)
+        
+        backgroundGradientLayerV.colors = colors
+        backgroundGradientLayerV.startPoint = CGPoint(x: 0.5, y: 1.0)
+        backgroundGradientLayerV.endPoint = CGPoint(x: 0.5, y: 0.0)
+        backgroundImageView.layer.addSublayer(backgroundGradientLayerV)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        backgroundImageView.pin.left(30%).bottom(20%).top().right()
+        backgroundGradientLayerH.pin.all()
+        backgroundGradientLayerV.pin.all()
     }
 }
