74787fe7709efd618c6b11179ef637d2ffe3f463
yongjun18 <yongjun18976@gmail.com>
2025-01-28 00:12:02 +0900

    part2 page3 vc > title, subtitle 세팅

--- AccessibilityPractice/Presentation/Part2/Part2Page3ViewController.swift
+++ AccessibilityPractice/Presentation/Part2/Part2Page3ViewController.swift
@@ -13,17 +13,40 @@ class Part2Page3ViewController: PresentationViewController {
     
     // MARK: - Properties
     
+    private var mainContentView = UIView()
+    private var titleLabel = UILabel()
+    private var subtitleLabel = UILabel()
     
+    private var innerContentView = UIView()
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.addSubview(mainContentView)
+        
+        titleLabel.text = "1. 접근성이란 무엇인가"
+        titleLabel.font = .systemFont(ofSize: 20, weight: .semibold)
+        titleLabel.textColor = .white
+        mainContentView.addSubview(titleLabel)
+        
+        subtitleLabel.text = "관련 법률"
+        subtitleLabel.font = .systemFont(ofSize: 15)
+        subtitleLabel.textColor = .white
+        mainContentView.addSubview(subtitleLabel)
+        
+        mainContentView.addSubview(innerContentView)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
+        mainContentView.pin.left(20).right(self.view.pin.safeArea).vertically()
+        titleLabel.pin.top(self.view.pin.safeArea).horizontally().marginTop(40).sizeToFit(.width)
+        subtitleLabel.pin.below(of: titleLabel).horizontally().marginTop(2).sizeToFit(.width)
+        
+        innerContentView.pin.below(of: subtitleLabel, aligned: .left)
+            .right().bottom(self.view.pin.safeArea).marginBottom(25)
     }
 }
