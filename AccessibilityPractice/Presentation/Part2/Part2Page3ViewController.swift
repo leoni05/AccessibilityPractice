d32678904743e815d974ddd62fb059c6aca58ccd
yongjun18 <yongjun18976@gmail.com>
2025-01-28 00:56:56 +0900

    관련 법률 label 세팅

--- AccessibilityPractice/Presentation/Part2/Part2Page3ViewController.swift
+++ AccessibilityPractice/Presentation/Part2/Part2Page3ViewController.swift
@@ -19,6 +19,17 @@ class Part2Page3ViewController: PresentationViewController {
     
     private var innerContentView = UIView()
     
+    private var lawContainerView = UIView()
+    
+    private var lawTitleLabel1 = UILabel()
+    private var lawDetailLabel1 = UILabel()
+    
+    private var lawTitleLabel2 = UILabel()
+    private var lawDetailLabel2 = UILabel()
+    
+    private var lawTitleLabel3 = UILabel()
+    private var lawDetailLabel3 = UILabel()
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -37,6 +48,38 @@ class Part2Page3ViewController: PresentationViewController {
         mainContentView.addSubview(subtitleLabel)
         
         mainContentView.addSubview(innerContentView)
+        
+        innerContentView.addSubview(lawContainerView)
+        
+        lawTitleLabel1.text = "장애인차별금지법 제21조 2항"
+        lawTitleLabel1.font = .systemFont(ofSize: 17)
+        lawTitleLabel1.textColor = .white
+        lawContainerView.addSubview(lawTitleLabel1)
+        
+        lawDetailLabel1.text = "정보통신·의사소통 등에서의 정당한 편의제공의무 <신설 2021. 7. 27.>"
+        lawDetailLabel1.font = .systemFont(ofSize: 13)
+        lawDetailLabel1.textColor = .white
+        lawContainerView.addSubview(lawDetailLabel1)
+        
+        lawTitleLabel2.text = "장애인차별금지법 시행령 제14조 5항"
+        lawTitleLabel2.font = .systemFont(ofSize: 17)
+        lawTitleLabel2.textColor = .white
+        lawContainerView.addSubview(lawTitleLabel2)
+        
+        lawDetailLabel2.text = "법 제21조제2항에 따라 행위자 등이 제공해야 하는 정당한 편의의 구체적인 내용 명시 <신설 2023. 3. 30.>"
+        lawDetailLabel2.font = .systemFont(ofSize: 13)
+        lawDetailLabel2.textColor = .white
+        lawContainerView.addSubview(lawDetailLabel2)
+        
+        lawTitleLabel3.text = "신규 웹 접근성 인증 심사지침 공개"
+        lawTitleLabel3.font = .systemFont(ofSize: 17)
+        lawTitleLabel3.textColor = .white
+        lawContainerView.addSubview(lawTitleLabel3)
+        
+        lawDetailLabel3.text = "웹 접근성 인증 심사 지침 변경(기존 24개 -> 33개) <2025. 1. 1. 시행>"
+        lawDetailLabel3.font = .systemFont(ofSize: 13)
+        lawDetailLabel3.textColor = .white
+        lawContainerView.addSubview(lawDetailLabel3)
     }
     
     override func viewDidLayoutSubviews() {
@@ -48,5 +91,18 @@ class Part2Page3ViewController: PresentationViewController {
         
         innerContentView.pin.below(of: subtitleLabel, aligned: .left)
             .right().bottom(self.view.pin.safeArea).marginBottom(25)
+        
+        lawContainerView.pin.left().right(20)
+        
+        lawTitleLabel1.pin.top().horizontally().sizeToFit(.width)
+        lawDetailLabel1.pin.below(of: lawTitleLabel1, aligned: .left).right().sizeToFit(.width).marginTop(5)
+        
+        lawTitleLabel2.pin.below(of: lawDetailLabel1, aligned: .left).right().sizeToFit(.width).marginTop(20)
+        lawDetailLabel2.pin.below(of: lawTitleLabel2, aligned: .left).right().sizeToFit(.width).marginTop(5)
+        
+        lawTitleLabel3.pin.below(of: lawDetailLabel2, aligned: .left).right().sizeToFit(.width).marginTop(20)
+        lawDetailLabel3.pin.below(of: lawTitleLabel3, aligned: .left).right().sizeToFit(.width).marginTop(5)
+        
+        lawContainerView.pin.wrapContent().left().vCenter()
     }
 }
