4ba0dc7906132733f9eebe70edfb383812523b70
yongjun18 <yongjun18976@gmail.com>
2025-01-27 19:00:53 +0900

    접근성 의미 세팅

--- AccessibilityPractice/Presentation/Part2/Part2Page2ViewController.swift
+++ AccessibilityPractice/Presentation/Part2/Part2Page2ViewController.swift
@@ -17,6 +17,12 @@ class Part2Page2ViewController: PresentationViewController {
     private var titleLabel = UILabel()
     private var subtitleLabel = UILabel()
     
+    private var innerContentView = UIView()
+    
+    private var definitionView = UIView()
+    private var definitionLabel = UILabel()
+    private var sourceLabel = UILabel()
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -24,7 +30,7 @@ class Part2Page2ViewController: PresentationViewController {
         
         self.view.addSubview(mainContentView)
         
-        titleLabel.text = "접근성이란 무엇인가"
+        titleLabel.text = "1. 접근성이란 무엇인가"
         titleLabel.font = .systemFont(ofSize: 20, weight: .semibold)
         titleLabel.textColor = .white
         mainContentView.addSubview(titleLabel)
@@ -34,6 +40,29 @@ class Part2Page2ViewController: PresentationViewController {
         subtitleLabel.textColor = .white
         mainContentView.addSubview(subtitleLabel)
         
+        mainContentView.addSubview(innerContentView)
+        
+        innerContentView.addSubview(definitionView)
+        
+        let definitionText = "접근성(接近性)은 사용자의 신체적 특성이나, 지역, 나이, 지식 수준, 기술, 체험과 같은 제한 사항을 고려하여 가능한 한 많은 사용자가 불편 없이 이용할 수 있도록 제품, 서비스를 만들어 제공하고 이를 평가 할 때 쓰이는 말이다."
+        let attrString = NSMutableAttributedString(string: definitionText)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 3
+        paragraphStyle.lineBreakStrategy = .hangulWordPriority
+        paragraphStyle.lineBreakMode = .byWordWrapping
+        attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attrString.length))
+        definitionLabel.attributedText = attrString
+        definitionLabel.numberOfLines = 0
+        definitionLabel.font = .systemFont(ofSize: 17)
+        definitionLabel.textColor = .white
+        definitionLabel.textAlignment = .center
+        definitionView.addSubview(definitionLabel)
+        
+        sourceLabel.text = "출처 : 위키백과"
+        sourceLabel.font = .systemFont(ofSize: 14)
+        sourceLabel.textColor = .white
+        sourceLabel.textAlignment = .right
+        definitionView.addSubview(sourceLabel)
     }
     
     override func viewDidLayoutSubviews() {
@@ -42,5 +71,13 @@ class Part2Page2ViewController: PresentationViewController {
         mainContentView.pin.left(20).right(self.view.pin.safeArea).vertically()
         titleLabel.pin.top(self.view.pin.safeArea).horizontally().marginTop(40).sizeToFit(.width)
         subtitleLabel.pin.below(of: titleLabel).horizontally().marginTop(2).sizeToFit(.width)
+        
+        innerContentView.pin.below(of: subtitleLabel, aligned: .left)
+            .right().bottom(self.view.pin.safeArea).marginBottom(25)
+        
+        definitionView.pin.horizontally(60)
+        definitionLabel.pin.top().horizontally().sizeToFit(.width)
+        sourceLabel.pin.below(of: definitionLabel, aligned: .left).right().sizeToFit(.width).marginTop(10)
+        definitionView.pin.wrapContent().center()
     }
 }
