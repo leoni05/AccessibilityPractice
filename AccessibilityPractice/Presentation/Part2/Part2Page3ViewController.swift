e2cf018a7978cfc507695526bde99d362f81937f
yongjun18 <yongjun18976@gmail.com>
2025-01-29 01:03:32 +0900

    part2 page3 vc > scrollview 세팅

--- AccessibilityPractice/Presentation/Part2/Part2Page3ViewController.swift
+++ AccessibilityPractice/Presentation/Part2/Part2Page3ViewController.swift
@@ -19,6 +19,9 @@ class Part2Page3ViewController: PresentationViewController {
     
     private var innerContentView = UIView()
     
+    private var scrollView = UIScrollView()
+    private var scrollViewLabel = UILabel()
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -37,6 +40,21 @@ class Part2Page3ViewController: PresentationViewController {
         mainContentView.addSubview(subtitleLabel)
         
         mainContentView.addSubview(innerContentView)
+        
+        scrollView.layer.cornerRadius = 5.0
+        scrollView.layer.borderWidth = 1.0
+        scrollView.layer.borderColor = UIColor.white.cgColor
+        scrollView.layer.masksToBounds = true
+        innerContentView.addSubview(scrollView)
+        
+        scrollViewLabel.text = "iOS Accessibility Updates"
+        scrollViewLabel.textColor = .black
+        scrollViewLabel.font = .systemFont(ofSize: 13)
+        scrollViewLabel.backgroundColor = .white
+        scrollViewLabel.textAlignment = .center
+        scrollViewLabel.layer.cornerRadius = 3.0
+        scrollViewLabel.layer.masksToBounds = true
+        innerContentView.addSubview(scrollViewLabel)
     }
     
     override func viewDidLayoutSubviews() {
@@ -48,5 +66,9 @@ class Part2Page3ViewController: PresentationViewController {
         
         innerContentView.pin.below(of: subtitleLabel, aligned: .left)
             .right(20).bottom(self.view.pin.safeArea).marginBottom(25)
+        
+        scrollView.pin.horizontally().top(30).bottom(20).maxWidth(500).justify(.center)
+        scrollViewLabel.pin.left(to: scrollView.edge.left).top(to: scrollView.edge.top)
+            .width(180).height(24).marginLeft(8).marginTop(-8)
     }
 }
