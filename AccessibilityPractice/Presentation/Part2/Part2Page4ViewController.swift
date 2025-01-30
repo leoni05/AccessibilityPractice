c73bb49569132f233514e5d4674edbeaad75c038
yongjun18 <yongjun18976@gmail.com>
2025-01-30 13:44:53 +0900

    apple video image view 	배치

--- AccessibilityPractice/Presentation/Part2/Part2Page4ViewController.swift
+++ AccessibilityPractice/Presentation/Part2/Part2Page4ViewController.swift
@@ -17,12 +17,30 @@ class Part2Page4ViewController: PresentationViewController {
     private var titleLabel = UILabel()
     private var subtitleLabel = UILabel()
     
+    private var imageContainerView = UIView()
+    private let imageRowCount = 6
+    private let imageColCount = 3
+    private let imageWidth = 150
+    private let imageHeight = 100
+    private var videoImages = Array<UIImageView>()
+    
     private var innerContentView = UIView()
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.addSubview(imageContainerView)
+        
+        for idx in 0..<imageRowCount*imageColCount {
+            let imageView = UIImageView()
+            imageView.image = UIImage(named: "AppleVideo\(String(format: "%02d", idx))")
+            imageView.contentMode = .scaleAspectFill
+            imageView.layer.masksToBounds = true
+            videoImages.append(imageView)
+            imageContainerView.addSubview(imageView)
+        }
         
         self.view.addSubview(mainContentView)
         
@@ -41,6 +59,19 @@ class Part2Page4ViewController: PresentationViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
+        imageContainerView.pin.right().vertically().width(400)
+        for c in 0..<imageColCount {
+            for r in 0..<imageRowCount {
+                let idx = c*imageRowCount + r
+                let x = CGFloat(c * (imageWidth + 10))
+                var y = CGFloat(r * (imageHeight + 10))
+                if c % 2 == 1 { y -= CGFloat((imageHeight + 10)/2) }
+                
+                videoImages[idx].pin.left(x).top(y)
+                    .width(CGFloat(imageWidth)).height(CGFloat(imageHeight))
+            }
+        }
         
         mainContentView.pin.left(20).right(self.view.pin.safeArea).vertically()
         titleLabel.pin.top(self.view.pin.safeArea).horizontally().marginTop(40).sizeToFit(.width)
