ac08af7a523aed2d6fcb74e23361802601917acf
yongjun18 <yongjun18976@gmail.com>
2025-01-30 12:53:35 +0900

    part2 page4 vc 세팅

---
+++ AccessibilityPractice/Presentation/Part2/Part2Page4ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  Part2Page4ViewController.swift
+//  AccessibilityPractice
+//
+//  Created by yongjun18 on 1/30/25.
+//
+
+import Foundation
+import UIKit
+import PinLayout
+
+class Part2Page4ViewController: PresentationViewController {
+    
+    // MARK: - Properties
+    
+    private var mainContentView = UIView()
+    private var titleLabel = UILabel()
+    private var subtitleLabel = UILabel()
+    
+    private var innerContentView = UIView()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.addSubview(mainContentView)
+        
+        titleLabel.text = "2. Apple의 접근성"
+        titleLabel.font = .systemFont(ofSize: 20, weight: .semibold)
+        titleLabel.textColor = .white
+        mainContentView.addSubview(titleLabel)
+        
+        subtitleLabel.text = "Apple의 노력"
+        subtitleLabel.font = .systemFont(ofSize: 15)
+        subtitleLabel.textColor = .white
+        mainContentView.addSubview(subtitleLabel)
+        
+        mainContentView.addSubview(innerContentView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        mainContentView.pin.left(20).right(self.view.pin.safeArea).vertically()
+        titleLabel.pin.top(self.view.pin.safeArea).horizontally().marginTop(40).sizeToFit(.width)
+        subtitleLabel.pin.below(of: titleLabel).horizontally().marginTop(2).sizeToFit(.width)
+        
+        innerContentView.pin.below(of: subtitleLabel, aligned: .left)
+            .right(20).bottom(self.view.pin.safeArea).marginBottom(25)
+    }
+}
