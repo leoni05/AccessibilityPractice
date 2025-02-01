11b1638bedc60eca6d3ceaeadd2107ecb1d2f6c3
yongjun18 <yongjun18976@gmail.com>
2025-02-01 18:01:38 +0900

    part3 page7 vc 레이아웃

--- AccessibilityPractice/Presentation/Part3/Part3Page7ViewController.swift
+++ AccessibilityPractice/Presentation/Part3/Part3Page7ViewController.swift
@@ -19,6 +19,26 @@ class Part3Page7ViewController: PresentationViewController {
     
     private var innerContentView = UIView()
     
+    private var exampleContainer = UIView()
+    private var beforeContainer = UIView()
+    private var beforeLabel = UILabel()
+    private var afterContainer = UIView()
+    private var afterLabel = UILabel()
+    
+    private var beforeButton1 = ExCustomButton3(titleString: "좋아요", imageName: "hand.thumbsup")
+    private var beforeButton2 = ExCustomButton3(titleString: "싫어요", imageName: "hand.thumbsdown")
+    private var beforeButton3 = ExCustomButton3(titleString: "알림", imageName: "bell")
+    
+    private var afterButton1 = ExCustomButton4(titleString: "좋아요", imageName: "hand.thumbsup")
+    private var afterButton2 = ExCustomButton4(titleString: "싫어요", imageName: "hand.thumbsdown")
+    private var afterButton3 = ExCustomButton4(titleString: "알림", imageName: "bell")
+    
+    private var propertyContainer = UIView()
+    private var propertyLabel = UILabel()
+    private var propertyDescLabel = UILabel()
+    private var editorScrollView = UIScrollView()
+    private var codeLabel = UILabel()
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -37,6 +57,75 @@ class Part3Page7ViewController: PresentationViewController {
         mainContentView.addSubview(subtitleLabel)
         
         mainContentView.addSubview(innerContentView)
+        
+        innerContentView.addSubview(exampleContainer)
+        
+        beforeContainer.layer.cornerRadius = 5.0
+        beforeContainer.layer.borderWidth = 1.0
+        beforeContainer.layer.borderColor = UIColor.white.cgColor
+        beforeContainer.layer.masksToBounds = true
+        exampleContainer.addSubview(beforeContainer)
+        
+        beforeLabel.text = "Before"
+        beforeLabel.textColor = .black
+        beforeLabel.font = .systemFont(ofSize: 13)
+        beforeLabel.backgroundColor = .white
+        beforeLabel.textAlignment = .center
+        beforeLabel.layer.cornerRadius = 3.0
+        beforeLabel.layer.masksToBounds = true
+        exampleContainer.addSubview(beforeLabel)
+        
+        afterContainer.layer.cornerRadius = 5.0
+        afterContainer.layer.borderWidth = 1.0
+        afterContainer.layer.borderColor = UIColor.white.cgColor
+        afterContainer.layer.masksToBounds = true
+        exampleContainer.addSubview(afterContainer)
+        
+        afterLabel.text = "After"
+        afterLabel.textColor = .black
+        afterLabel.font = .systemFont(ofSize: 13)
+        afterLabel.backgroundColor = .white
+        afterLabel.textAlignment = .center
+        afterLabel.layer.cornerRadius = 3.0
+        afterLabel.layer.masksToBounds = true
+        exampleContainer.addSubview(afterLabel)
+        
+        beforeContainer.addSubview(beforeButton1)
+        beforeContainer.addSubview(beforeButton2)
+        beforeContainer.addSubview(beforeButton3)
+        
+        afterContainer.addSubview(afterButton1)
+        afterContainer.addSubview(afterButton2)
+        afterContainer.addSubview(afterButton3)
+        
+        innerContentView.addSubview(propertyContainer)
+        
+        propertyLabel.text = "accessibilityTraits"
+        propertyLabel.font = .systemFont(ofSize: 17)
+        propertyLabel.textColor = .white
+        propertyContainer.addSubview(propertyLabel)
+        
+        propertyDescLabel.text = "접근성 요소의 특징을 나타남(button, image, link 등)."
+        propertyDescLabel.font = .systemFont(ofSize: 14)
+        propertyDescLabel.textColor = .white
+        propertyDescLabel.numberOfLines = 3
+        propertyContainer.addSubview(propertyDescLabel)
+        
+        editorScrollView.layer.cornerRadius = 5.0
+        editorScrollView.layer.borderWidth = 1.0
+        editorScrollView.layer.borderColor = UIColor.white.cgColor
+        editorScrollView.layer.masksToBounds = true
+        propertyContainer.addSubview(editorScrollView)
+        
+        codeLabel.text = """
+self.isAccessibilityElement = true
+self.accessibilityLabel = titleString
+self.accessibilityTraits = .button
+"""
+        codeLabel.font = .systemFont(ofSize: 14)
+        codeLabel.textColor = .white
+        codeLabel.numberOfLines = 0
+        editorScrollView.addSubview(codeLabel)
     }
     
     override func viewDidLayoutSubviews() {
@@ -48,5 +137,34 @@ class Part3Page7ViewController: PresentationViewController {
         
         innerContentView.pin.below(of: subtitleLabel, aligned: .left)
             .right(20).bottom(self.view.pin.safeArea).marginBottom(25)
+        
+        beforeContainer.pin.top().left().width(250).height(80)
+        beforeLabel.pin.left(to: beforeContainer.edge.left).top(to: beforeContainer.edge.top)
+            .width(60).height(24).marginLeft(8).marginTop(-12)
+        
+        afterContainer.pin.below(of: beforeContainer).left().width(250).height(80).marginTop(20)
+        afterLabel.pin.left(to: afterContainer.edge.left).top(to: afterContainer.edge.top)
+            .width(60).height(24).marginLeft(8).marginTop(-12)
+        
+        beforeButton2.pin.center().size(48)
+        beforeButton1.pin.before(of: beforeButton2, aligned: .center).size(48).marginRight(10)
+        beforeButton3.pin.after(of: beforeButton2, aligned: .center).size(48).marginLeft(10)
+        
+        afterButton2.pin.center().size(48)
+        afterButton1.pin.before(of: afterButton2, aligned: .center).size(48).marginRight(10)
+        afterButton3.pin.after(of: afterButton2, aligned: .center).size(48).marginLeft(10)
+        
+        exampleContainer.pin.wrapContent().right().vCenter()
+        
+        propertyContainer.pin.left().right(to: exampleContainer.edge.left).marginRight(30)
+        propertyLabel.pin.top().horizontally().sizeToFit(.width)
+        propertyDescLabel.pin.below(of: propertyLabel).horizontally().marginTop(10).sizeToFit(.width)
+        editorScrollView.pin.below(of: propertyDescLabel).horizontally().height(120)
+            .marginTop(10)
+        propertyContainer.pin.wrapContent().left().vCenter()
+        
+        codeLabel.pin.top(12).horizontally(12).sizeToFit(.width)
+        editorScrollView.contentSize = CGSize(width: editorScrollView.bounds.width,
+                                              height: codeLabel.frame.maxY + 12)
     }
 }
