ff3b62459fafd318ca410f6b3b3100d1a69fa24e
yongjun18 <yongjun18976@gmail.com>
2025-02-23 19:07:47 +0900

    part1 page3 vc lawLabel 배치 방법 변경

--- AccessibilityPractice/Presentation/Part1/Part1Page3ViewController.swift
+++ AccessibilityPractice/Presentation/Part1/Part1Page3ViewController.swift
@@ -21,12 +21,15 @@ class Part1Page3ViewController: PresentationViewController {
     
     private var lawContainerView = UIView()
     
+    private var lawView1 = UIView()
     private var lawTitleLabel1 = UILabel()
     private var lawDetailLabel1 = UILabel()
     
+    private var lawView2 = UIView()
     private var lawTitleLabel2 = UILabel()
     private var lawDetailLabel2 = UILabel()
     
+    private var lawView3 = UIView()
     private var lawTitleLabel3 = UILabel()
     private var lawDetailLabel3 = UILabel()
     
@@ -51,41 +54,45 @@ class Part1Page3ViewController: PresentationViewController {
         
         innerContentView.addSubview(lawContainerView)
         
+        lawContainerView.addSubview(lawView1)
+        lawContainerView.addSubview(lawView2)
+        lawContainerView.addSubview(lawView3)
+        
         lawTitleLabel1.text = "장애인차별금지법 제21조 2항"
         lawTitleLabel1.numberOfLines = 0
         lawTitleLabel1.font = .systemFont(ofSize: 17)
         lawTitleLabel1.textColor = .white
-        lawContainerView.addSubview(lawTitleLabel1)
+        lawView1.addSubview(lawTitleLabel1)
         
         lawDetailLabel1.text = "정보통신·의사소통 등에서의 정당한 편의제공의무 <신설 2021. 7. 27.>"
         lawDetailLabel1.numberOfLines = 0
         lawDetailLabel1.font = .systemFont(ofSize: 13)
         lawDetailLabel1.textColor = .white
-        lawContainerView.addSubview(lawDetailLabel1)
+        lawView1.addSubview(lawDetailLabel1)
         
         lawTitleLabel2.text = "장애인차별금지법 시행령 제14조 5항"
         lawTitleLabel2.numberOfLines = 0
         lawTitleLabel2.font = .systemFont(ofSize: 17)
         lawTitleLabel2.textColor = .white
-        lawContainerView.addSubview(lawTitleLabel2)
+        lawView2.addSubview(lawTitleLabel2)
         
         lawDetailLabel2.text = "법 제21조제2항에 따라 행위자 등이 제공해야 하는 정당한 편의의 구체적인 내용 명시 <신설 2023. 3. 30.>"
         lawDetailLabel2.numberOfLines = 0
         lawDetailLabel2.font = .systemFont(ofSize: 13)
         lawDetailLabel2.textColor = .white
-        lawContainerView.addSubview(lawDetailLabel2)
+        lawView2.addSubview(lawDetailLabel2)
         
         lawTitleLabel3.text = "신규 웹 접근성 인증 심사지침 공개"
         lawTitleLabel3.numberOfLines = 0
         lawTitleLabel3.font = .systemFont(ofSize: 17)
         lawTitleLabel3.textColor = .white
-        lawContainerView.addSubview(lawTitleLabel3)
+        lawView3.addSubview(lawTitleLabel3)
         
         lawDetailLabel3.text = "웹 접근성 인증 심사 지침 변경(기존 24개 -> 33개) <시행 2025. 1. 1.>"
         lawDetailLabel3.numberOfLines = 0
         lawDetailLabel3.font = .systemFont(ofSize: 13)
         lawDetailLabel3.textColor = .white
-        lawContainerView.addSubview(lawDetailLabel3)
+        lawView3.addSubview(lawDetailLabel3)
     }
     
     override func viewDidLayoutSubviews() {
@@ -99,15 +106,21 @@ class Part1Page3ViewController: PresentationViewController {
             .right().bottom(self.view.pin.safeArea).marginBottom(25)
         
         lawContainerView.pin.horizontally()
+        lawView1.pin.horizontally()
+        lawView2.pin.horizontally()
+        lawView3.pin.horizontally()
         
         lawTitleLabel1.pin.top().horizontally().sizeToFit(.width)
         lawDetailLabel1.pin.below(of: lawTitleLabel1, aligned: .left).right().sizeToFit(.width).marginTop(5)
+        lawView1.pin.wrapContent().top().hCenter()
         
-        lawTitleLabel2.pin.below(of: lawDetailLabel1, aligned: .left).right().sizeToFit(.width).marginTop(20)
+        lawTitleLabel2.pin.top().horizontally().sizeToFit(.width)
         lawDetailLabel2.pin.below(of: lawTitleLabel2, aligned: .left).right().sizeToFit(.width).marginTop(5)
+        lawView2.pin.wrapContent().below(of: lawView1).hCenter().marginTop(20)
         
-        lawTitleLabel3.pin.below(of: lawDetailLabel2, aligned: .left).right().sizeToFit(.width).marginTop(20)
+        lawTitleLabel3.pin.top().horizontally().sizeToFit(.width)
         lawDetailLabel3.pin.below(of: lawTitleLabel3, aligned: .left).right().sizeToFit(.width).marginTop(5)
+        lawView3.pin.wrapContent().below(of: lawView2).hCenter().marginTop(20)
         
         lawContainerView.pin.wrapContent().center()
     }
