6dd20b68374957c1085bdf32dafe92d1b478fa3f
yongjun18 <yongjun18976@gmail.com>
2025-02-28 13:29:32 +0900

    part3 page4 vc > appear 애니메이션 구현

--- AccessibilityPractice/Presentation/Part3/Part3Page4ViewController.swift
+++ AccessibilityPractice/Presentation/Part3/Part3Page4ViewController.swift
@@ -29,6 +29,8 @@ class Part3Page4ViewController: PresentationViewController {
     private var propertyLabels = Array<UILabel>()
     private var labelContainer = UIView()
     
+    private var isWillAppear = false
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -81,5 +83,37 @@ class Part3Page4ViewController: PresentationViewController {
             }
         }
         labelContainer.pin.wrapContent().left().vCenter()
+        
+        if isWillAppear {
+            readyForAppearAnimation()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        isWillAppear = true
+        readyForAppearAnimation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        isWillAppear = false
+        UIView.animate(withDuration: 0.3) {
+            self.innerContentView.alpha = 1.0
+            self.innerContentView.pin.below(of: self.subtitleLabel, aligned: .left)
+                .right().bottom(self.view.pin.safeArea).marginBottom(25)
+        }
+    }
+}
+
+// MARK: - Private Extensions
+
+private extension Part3Page4ViewController {
+    func readyForAppearAnimation() {
+        innerContentView.alpha = 0.0
+        innerContentView.pin.below(of: subtitleLabel, aligned: .left)
+            .right().bottom(self.view.pin.safeArea).marginBottom(25).marginLeft(-10)
     }
 }
